f1906c30cee290bdf979a1bd1c378b6f7d516cf1
AVSSAM <sm201211d@gmail.com>
2021-07-29 15:29:47 +0530

    otp authetication added

--- app/src/main/java/com/example/nanochat/MainActivity.java
+++ app/src/main/java/com/example/nanochat/MainActivity.java
@@ -1,14 +1,128 @@
 package com.example.nanochat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.hbb20.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText getPhoneNumber;
+    android.widget.Button sendOTP;
+    CountryCodePicker countryCodePicker;
+    String countryCode;
+    String phoneNumber;
+
+    FirebaseAuth firebaseAuth;
+    ProgressBar progressBar;
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks callbacks;
+    String codeSent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countryCodePicker = findViewById(R.id.countrycodepicker);
+        sendOTP = findViewById(R.id.sendotpbutton);
+        getPhoneNumber = findViewById(R.id.getphonenumber);
+        progressBar = findViewById(R.id.progressbarofmain);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        countryCode = countryCodePicker.getSelectedCountryCodeWithPlus();
+
+        countryCodePicker.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected() {
+                countryCode = countryCodePicker.getSelectedCountryCodeWithPlus();
+            }
+        });
+
+        sendOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String number;
+                number = getPhoneNumber.getText().toString().trim();
+
+                if(number.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please Enter Your Phone Number",Toast.LENGTH_SHORT).show();
+
+                }
+
+                else if(number.length()<9){
+                    Toast.makeText(getApplicationContext(),"Please Correct Your Phone Number",Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    progressBar.setVisibility(View.VISIBLE);
+                    phoneNumber = countryCode+number;
+
+                    PhoneAuthOptions phoneAuthOptions = PhoneAuthOptions.newBuilder(firebaseAuth)
+                            .setPhoneNumber(phoneNumber)
+                            .setTimeout(60L, TimeUnit.SECONDS)
+                            .setActivity(MainActivity.this)
+                            .setCallbacks(callbacks)
+                            .build();
+
+                    PhoneAuthProvider.verifyPhoneNumber(phoneAuthOptions);
+                }
+            }
+        });
+
+        callbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                //auto fetch otp
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                Toast.makeText(getApplicationContext(),"Code is sent",Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.INVISIBLE);
+                codeSent = s;
+                Intent intent= new Intent(MainActivity.this,OtpAuthenticationActivity.class);
+                intent.putExtra("otp",codeSent);
+                startActivity(intent);
+
+
+            }
+        };
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(FirebaseAuth.getInstance().getCurrentUser() != null){
+            Intent intent= new Intent(MainActivity.this,ChatActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+
+        }
     }
 }
--- app/src/main/java/com/example/nanochat/OtpAuthenticationActivity.java
+++ app/src/main/java/com/example/nanochat/OtpAuthenticationActivity.java
@@ -1,14 +1,95 @@
 package com.example.nanochat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 public class OtpAuthenticationActivity extends AppCompatActivity {
+
+    TextView changeNumber;
+    EditText getOTP;
+    android.widget.Button verifyOTP;
+    String enteredOTP;
+
+    FirebaseAuth firebaseAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_authentication);
+
+        changeNumber = findViewById(R.id.changenumber);
+        verifyOTP = findViewById(R.id.verifyotp);
+        getOTP = findViewById(R.id.getotp);
+        progressBar = findViewById(R.id.progressbarofotpauth);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        changeNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OtpAuthenticationActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        verifyOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                enteredOTP = getOTP.getText().toString();
+                String codeRecieved= getIntent().getStringExtra("otp");
+
+                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codeRecieved,enteredOTP);
+                signInWithPhoneAuthCredentials(credential);
+
+
+            }
+        });
+
+
+
+    }
+
+    private void signInWithPhoneAuthCredentials(PhoneAuthCredential credential){
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()) {
+
+                    progressBar.setVisibility(View.INVISIBLE);
+                    Toast.makeText(getApplicationContext(), "Login Succeed", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(OtpAuthenticationActivity.this, ChatActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    if(task.getException() instanceof FirebaseAuthInvalidCredentialsException){
+                        Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.INVISIBLE);
+
+                    }
+                }
+
+            }
+        });
+
     }
 }
