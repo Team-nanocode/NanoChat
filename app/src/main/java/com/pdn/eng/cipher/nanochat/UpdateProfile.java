c266bd4a006eb0fb58381fd27826f7c164f4f21f
NuwanHarsha <nuwan.harshamatrix@gmail.com>
2021-08-07 14:59:30 +0530

    Menu | Update Profile | Chats

---
+++ app/src/main/java/com/pdn/eng/cipher/nanochat/UpdateProfile.java
@@ -0,0 +1,14 @@
+package com.pdn.eng.cipher.nanochat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_profile);
+    }
+}
